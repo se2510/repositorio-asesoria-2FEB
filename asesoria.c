4d3e248734f975b900e7bea27b7fbfd091b23606
Deivi <elias10david@hotmail.com>
2024-02-02 10:17:06 -0600

    Añadimos cositas y funciones :D

--- asesoria.c
+++ asesoria.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
+int suma(int a, int b);
+
 int main(){
     printf("hola oscars :D");
-    printf("Este es mi segundo commit");
+    printf("\nEste es mi segundo commit\n");
 
     int a=0;
     scanf("%d",&a);
-    printf("Tu variable es: ",a);
+    printf("\nTu variable es: %d\n", a);
+
+    int resultado;
+    resultado = suma(2,4);
+    printf("\nEl resultado de la suma es: %d", resultado);
+}
+
+int suma(int a, int b){
+    //int x,y;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    return a+b;
 }
